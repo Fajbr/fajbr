73f5c822da0469a1036fe31be8d2c6ed4640e029
Daniel Gabzdyl <daniel.gabzdyl@eman.cz>
2025-02-15 16:53:59 +0100

    chore: Initialize new SwiftUI project
    - Created a new SwiftUI project with default settings.
    
    This commit represents the starting point for development.

---
+++ fajbr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fajbr
+//
+//  Created by Daniel Gabzdyl on 15.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fajbr/fajbrApp.swift
@@ -0,0 +1,17 @@
+//
+//  fajbrApp.swift
+//  fajbr
+//
+//  Created by Daniel Gabzdyl on 15.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct fajbrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
